fd51184b38f375b316bec180085c1dbe77dbc4a7
salsichasausage <pacude40metro@gmail.com>
2026-09-16 01:09:34 -0300

    change the layout for md architecture

--- web-sgcf/src/app/layout/layout.ts
+++ web-sgcf/src/app/layout/layout.ts
@@ -11,7 +11,7 @@ import {
   Target,
   UserRound,
   UsersRound,
-} from 'lucide';
+} from 'lucide-angular';
 import { AppIcon } from '../shared/app-icon';
 import { AuthService } from '../service/auth.service';
 
@@ -20,7 +20,7 @@ import { AuthService } from '../service/auth.service';
   imports: [AppIcon, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
-  changeDetection: ChangeDetectionStrategy.Eager,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Layout {
   protected readonly sidebarOpen = signal(false);
